4b87fce1e91643583f282439981a672653309318
Vinicius Vale <vinicius.aquino@gmail.com>
2015-08-30 19:18:34 -0300

    Prepare structure

---
+++ src/main/java/br/aquino/App.java
@@ -0,0 +1,13 @@
+package br.aquino;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
